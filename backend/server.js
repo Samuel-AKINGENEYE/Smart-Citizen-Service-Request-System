074c90d770cc0f24105fb43e2e595071753e77d7
Samuel AKINGENEYE <freshtalent491@gmail.com>
2025-12-13 14:39:46 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 console.log('server.js execution started');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const requestRoutes = require('./routes/requests');
 const db = require('./db');
 
 const app = express();
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 console.log('server.js loaded');
-
-app.use('/requests', requestRoutes);
 
 app.get('/', (req, res) => res.send('Backend is running!'));
 
