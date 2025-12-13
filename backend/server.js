0f041fa596eee37c6a722a4f3f2d24d1f8ed267d
Samuel AKINGENEYE <freshtalent491@gmail.com>
2025-12-13 13:51:47 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const requestRoutes = require('./routes/requests');
+const db = require('./db'); // require db to trigger connection
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Simple log to ensure server.js is running
+console.log('server.js loaded');
 
 app.use('/requests', requestRoutes);
 
