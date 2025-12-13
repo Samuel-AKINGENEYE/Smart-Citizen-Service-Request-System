b8ef6481faab3d3f0a6cc9657e2a186af5d6e40b
Samuel AKINGENEYE <freshtalent491@gmail.com>
2025-12-13 12:00:10 +0200

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,46 @@
+const API_URL = 'http://localhost:3000/requests';
 
+document.getElementById('reportForm').addEventListener('submit', e => {
+    e.preventDefault();
+    const data = {
+        citizen_name: document.getElementById('name').value,
+        contact_info: document.getElementById('contact').value,
+        issue_type: document.getElementById('issueType').value,
+        location: document.getElementById('location').value,
+        description: document.getElementById('description').value
+    };
+
+    fetch(API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(() => {
+        alert('Request submitted!');
+        document.getElementById('reportForm').reset();
+        loadRequests();
+    });
+});
+
+// Load all requests
+function loadRequests() {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(requests => {
+        const container = document.getElementById('requestsList');
+        container.innerHTML = '';
+        requests.forEach(r => {
+            container.innerHTML += `
+                <div>
+                    <strong>${r.citizen_name} (${r.contact_info})</strong><br>
+                    Issue: ${r.issue_type}<br>
+                    Location: ${r.location}<br>
+                    Status: ${r.status}<br>
+                    <p>${r.description}</p>
+                </div><hr>`;
+        });
+    });
+}
+
+loadRequests();
