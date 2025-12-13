c607b92cfe1adb9454b53941e247171b4295820f
Samuel AKINGENEYE <freshtalent491@gmail.com>
2025-12-13 11:57:59 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const requestRoutes = require('./routes/requests');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/requests', requestRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
