d5542192c3b241aa7901c01b13e50030527a3622
Samuel AKINGENEYE <freshtalent491@gmail.com>
2025-12-13 11:57:25 +0200

    Update request.js

--- backend/routes/request.js
+++ backend/routes/request.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/requestController');
 
+router.get('/', controller.getAllRequests);
+router.post('/', controller.createRequest);
+router.put('/:id', controller.updateStatus);
+
+module.exports = router;
