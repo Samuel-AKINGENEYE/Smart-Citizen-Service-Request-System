8f376f77038e9ad4ece59f8ca7e5b76662808a9b
Samuel AKINGENEYE <freshtalent491@gmail.com>
2025-12-13 14:12:46 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,17 +2,14 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'samuel',           // your MySQL username
-    password: 'Samuel@2025!', // your MySQL password
+    user: 'samuel',
+    password: 'Samuel@2025!',
     database: 'smart_city'
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('DB Connection FAILED:', err.message);
-    } else {
-        console.log('Connected to MySQL database.');
-    }
+db.connect(err => {
+    if (err) console.error('DB Connection FAILED:', err.message);
+    else console.log('Connected to MySQL database.');
 });
 
 module.exports = db;
